28a7bb04af3f8dcbf15f81d95942fe034b97db47
Marms <delmotteflorent@sfr.fr>
2019-05-19 14:36:33 +0200

    feat: add AuthGuard on lazy loaded feature

--- src/app/auth/auth-guard.ts
+++ src/app/auth/auth-guard.ts
@@ -1,14 +1,19 @@
-import {ActivatedRoute, ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular/router';
+import {ActivatedRoute, ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot} from '@angular/router';
 import {AuthService} from './auth.service';
 import {Injectable} from '@angular/core';
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
-  constructor(private authSvc: AuthService) {}
+  constructor(private authSvc: AuthService) {
+  }
 
   canActivate(activeRouteSnap: ActivatedRouteSnapshot,
               routerStateSnap: RouterStateSnapshot) {
+    return this.authSvc.isAuthentificated();
+  }
+
+  canLoad(route: Route) {
     return this.authSvc.isAuthentificated();
   }
 }
