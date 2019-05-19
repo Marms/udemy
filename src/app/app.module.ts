c0218c56588fbc2f885f8046b9ee9de8e906fbec
Marms <delmotteflorent@sfr.fr>
2019-05-19 15:34:33 +0200

    move provider from app.module to core.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { RecipeService } from './recipes/recipe.service';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import {HttpModule} from '@angular/http';
-import {DataStorageService} from './shared/data-storage.service';
-import {AuthService} from './auth/auth.service';
-import {AuthGuard} from './auth/auth-guard';
 import {SharedModule} from './shared/shared.module';
 import {ShoppingModule} from './shopping-list/shopping.module';
 import {AuthModule} from './auth/auth.module';
@@ -29,7 +24,7 @@ import {CoreModule} from './core/core.module';
     SharedModule,
     CoreModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,11 @@ import {HomeComponent} from './home/home.component';
 import {SharedModule} from '../shared/shared.module';
 import {AppRoutingModule} from '../app-routing.module';
 import {NgModule} from '@angular/core';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
+import {RecipeService} from '../recipes/recipe.service';
+import {DataStorageService} from '../shared/data-storage.service';
+import {AuthService} from '../auth/auth.service';
+import {AuthGuard} from '../auth/auth-guard';
 
 @NgModule({
   declarations: [
@@ -16,6 +21,7 @@ import {NgModule} from '@angular/core';
   exports: [
     AppRoutingModule,
     HeaderComponent
-  ]
+  ],
+  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard]
 })
 export class CoreModule {}
