431fdaee6e4628c2deb57cad85fd91ac7202c708
Marms <delmotteflorent@sfr.fr>
2019-05-25 12:41:56 +0200

    feat definition of Shopping list action and finalize Shopping list reducer

---
+++ src/app/shopping-list/store/shopping-list.reducers.ts
@@ -0,0 +1,18 @@
+import {Action} from '@ngrx/store';
+import {Ingredient} from '../../shared/ingredient.model';
+import * as ShoppingListAction from './shopping-list.actions';
+
+const initialState = {ingredients: []}; // etat par defaut
+
+
+export function shoppingListReducers(state = initialState, action: ShoppingListAction.ShoppingListActions) {
+
+  switch (action.type) {
+    case ShoppingListAction.ADD_INGREDIENT: // je veux update mon etat en ajoutant un ingredient
+      return {
+        ...state, ingredients: [...state.ingredients, action.payload]
+      };
+    default:
+      return state;
+  }
+}
